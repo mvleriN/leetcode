8c67ee8c51748f4825f55ce49dd1e50afdea992c
Александр Ивлев <ivlevaleksandr4@gmail.com>
2023-12-19 12:30:51 +0300

    dp

--- 139. Word Break/139. Word Break/139. Word Break.cpp
+++ 139. Word Break/139. Word Break/139. Word Break.cpp
@@ -19,7 +19,7 @@ int main() {
     canSplit[0] = true;
 
     for (int prefixLen = 1; prefixLen <= n; ++prefixLen) {
-        string prefix = s.substr(0, prefixLen); //string view when using class
+        string prefix = s.substr(0, prefixLen); //string_view when using class
         for (const string& word : wordDict) {
             if (((int)word.size() <= prefixLen) && canSplit[prefixLen - (int)word.size()]) {
                 if (word == prefix.substr(prefixLen - (int)word.size())) {
