c426703cb3ff6e1272c3660cfd7a6d115408d558
Александр Ивлев <ivlevaleksandr4@gmail.com>
2023-12-19 15:12:53 +0300

    dp

---
+++ 377. Combination Sum IV/377. Combination Sum IV/377. Combination Sum IV.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector <int> nums(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> nums[i];
+    }
+    int target;
+    cin >> target;
+    vector <unsigned int> dp(target + 1, 0);
+    dp[0] = 1;
+    for (int sum = 1; sum <= target; ++sum) {
+        for (int num : nums) {
+            if (sum - num >= 0) {
+                dp[sum] += dp[sum - num];
+            }
+        }
+    }
+    cout << dp[target];
+    return 0;
+}
