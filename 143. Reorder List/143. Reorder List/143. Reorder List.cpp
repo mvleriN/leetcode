3b4494eab19f343d884e873b559edba558ac5e33
Александр Ивлев <ivlevaleksandr4@gmail.com>
2023-12-20 13:38:17 +0300

    LL

---
+++ 143. Reorder List/143. Reorder List/143. Reorder List.cpp
@@ -0,0 +1,27 @@
+﻿/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        if (!head || !head->next || !head->next->next) {
+            return;
+        }
+        ListNode* node = head;
+        while (node->next->next != nullptr) {
+            node = node->next;
+        }
+
+        node->next->next = head->next;
+        head->next = node->next;
+        node->next = nullptr;
+        reorderList(head->next->next);
+    }
+};
---
+++ 19. Remove Nth Node From End of List/19. Remove Nth Node From End of List/19. Remove Nth Node From End of List.cpp
@@ -0,0 +1,32 @@
+﻿/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* node = head;
+        int size = 0;
+
+        while (node != nullptr) {
+            node = node->next;
+            size++;
+        }
+        if (size == 1 || size == n)
+            return head->next;
+
+        node = head;
+        for (int i = 0; i < size - n - 1; ++i) {
+            node = node->next;
+        }
+
+        node->next = node->next->next;
+        return head;
+    }
+};
