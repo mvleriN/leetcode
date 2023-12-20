8c4738bfcd1d2f1b2b8928a642b28c440710f82f
Александр Ивлев <ivlevaleksandr4@gmail.com>
2023-12-20 19:30:13 +0300

    matrix

---
+++ 73. Set Matrix Zeroes/73. Set Matrix Zeroes/73. Set Matrix Zeroes.cpp
@@ -0,0 +1,45 @@
+﻿#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+	vector <vector <int>> matrix(n, vector <int>(m));
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            cin >> matrix[i][j];
+        }
+    }
+    vector <vector <bool>> visited(n, vector <bool>(m, false));
+    int idx = 0;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            if (!visited[i][j] && matrix[i][j] == 0) {
+                for (int k = 0; k < m; ++k) {
+                    if (matrix[i][k] != 0) {
+                        visited[i][k] = true;
+                    }
+                    matrix[i][k] = 0;
+                }
+                for (int k = 0; k < n; ++k) {
+                    if (matrix[k][j] != 0) {
+                        visited[k][j] = true;
+                    }
+                    matrix[k][j] = 0;
+                }
+            }
+            visited[i][j] = true;
+        }
+    }
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            cout << matrix[i][j] << ' ';
+        }
+        cout << endl;
+    }
+
+	return 0;
+}
