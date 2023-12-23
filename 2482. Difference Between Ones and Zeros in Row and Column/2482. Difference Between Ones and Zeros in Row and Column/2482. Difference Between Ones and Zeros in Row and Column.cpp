c82ee1742e5677d064faa4015e1c1cdc36e9ff53
mvleriN <ivlevaleksandr4@gmail.com>
2023-12-23 15:17:14 +0300

    dp

---
+++ 2482. Difference Between Ones and Zeros in Row and Column/2482. Difference Between Ones and Zeros in Row and Column/2482. Difference Between Ones and Zeros in Row and Column.cpp
@@ -0,0 +1,29 @@
+﻿static const int ZERO = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return 0;
+    }();
+
+    class Solution {
+    public:
+        vector<vector<int>> onesMinusZeros(vector<vector<int>>& grid) {
+            int n = grid.size();
+            int m = grid[0].size();
+            vector <int> dpRows(n, 0), dpCols(m, 0);
+
+            for (int i = 0; i < n; ++i) {
+                for (int j = 0; j < m; ++j) {
+                    if (grid[i][j] == 1) {
+                        dpRows[i]++;
+                        dpCols[j]++;
+                    }
+                }
+            }
+            for (int i = 0; i < n; ++i) {
+                for (int j = 0; j < m; ++j) {
+                    grid[i][j] = 2 * (dpRows[i] + dpCols[j]) - n - m;
+                }
+            }
+            return grid;
+        }
+    };
