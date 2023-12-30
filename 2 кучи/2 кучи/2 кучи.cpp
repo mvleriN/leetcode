fdbc34aa6652edcfc20bd2c5ef3abd398b16d470
Александр Ивлев <ivlevaleksandr4@gmail.com>
2023-12-30 17:22:32 +0300

    Yandex

---
+++ 2 кучи/2 кучи/2 кучи.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+	if (n > m) {
+		swap(n, m);
+	}
+	
+	if (n == 0 || (n == 1 && m == 1)) {
+		cout << "Loose";
+	}
+	else {
+		if ((n == 1 && m == 2) || (n == 2 && m == 2)) {
+			cout << "Win";
+		}
+	}
+	cout << "Win";
+
+	return 0;
+}
---
+++ Бронирование переговорки/Бронирование переговорки/Бронирование переговорки.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct Interval {
+	int l;
+	int r;
+};
+
+bool cmp(Interval a, Interval b) {
+	return a.r < b.r;
+}
+int main() {
+	int n;
+	cin >> n;
+	vector <Interval> a(n);
+	for (int i = 0; i < n; ++i) {
+		cin >> a[i].l >> a[i].r;
+	}
+
+	sort(a.begin(), a.end(), cmp);
+	int ans = 1;
+	int curr = a[0].r;
+	for (int i = 1; i < n; ++i) {
+		if (a[i].l > curr) {
+			ans++;
+			curr = a[i].r;
+		}
+	}
+
+	cout << ans;
+	return 0;
+}
